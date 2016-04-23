4f5f57acb08d6e36384c3ede4367c21e409558c4
EC2 Default User <ec2-user@ip-172-31-39-28.us-west-2.compute.internal>
2016-04-23 12:34:13 +0000

    6

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send("Hello from container land!");
+  res.send("Hello from container land - it's Ship time on shippable...Heroku, I am sending you a Docker!");
 });
 
 var server = app.listen(process.env.PORT, function () {
